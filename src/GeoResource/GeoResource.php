4402431ecff55a01733c5c4a766dbbe539970840
sugarfixx <ingar.torsrud@me.com>
2021-02-28 17:43:29 +0100

    Implement strategy

--- src/GeoResource/GeoResource.php
+++ src/GeoResource/GeoResource.php
@@ -6,5 +6,17 @@ namespace GeoResource;
 
 class GeoResource
 {
-    //
+    private $strategy;
+
+    public function __construct(Strategy $strategy)
+    {
+        $this->strategy = $strategy;
+    }
+
+    public function setStrategy(Strategy $strategy)
+    {
+        $this->strategy = $strategy;
+    }
+
+    
 }
