57dbf9ad9c34a5322d37775ca3366aaa908f7bf0
sugarfixx <ingar.torsrud@me.com>
2021-03-01 09:33:19 +0100

    Distance

---
+++ src/GeoResource/Distance.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace GeoResource;
+
+
+class Distance
+{
+    private $base;
+
+    private $destination;
+
+    /**
+     * @return mixed
+     */
+    public function getBase()
+    {
+        return $this->base;
+    }
+
+    /**
+     * @param Location $base
+     */
+    public function setBase(Location $base): void
+    {
+        $this->base = $base;
+    }
+
+    /**
+     * @return Location
+     */
+    public function getDestination() : Location
+    {
+        return $this->destination;
+    }
+
+    /**
+     * @param Location $destination
+     */
+    public function setDestination(Location $destination): void
+    {
+        $this->destination = $destination;
+    }
+
+
+    public function betweenBaseAndDestination()
+    {
+        //  Pythagorean theorem
+        // d=√((x_2-x_1)²+(y_2-y_1)²)
+
+        $x = $this->base->latitude - $this->destination->latitude;
+        $y = $this->base->longitude - $this->destination->longitude;
+
+        return sqrt(($x**2) + ($y**2));
+    }
+}
