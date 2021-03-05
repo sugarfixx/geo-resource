d51626bdc768b81492bec21f4df8359ffe31a318
sugarfixx <ingar.torsrud@me.com>
2021-03-05 07:56:56 +0100

    Add setters and getters to Result set

--- src/GeoResource/ResultSet.php
+++ src/GeoResource/ResultSet.php
@@ -6,5 +6,29 @@ namespace GeoResource;
 
 class ResultSet
 {
+    private $data;
+
+    /**
+     * @return ResultSet
+     */
+    public function getData() : ResultSet
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param array $data
+     * @return ResultSet
+     */
+    public function setData(array $data) : ResultSet
+    {
+        $this->data = $data;
+        return $this;
+    }
+
+    public function addData(array $data) : void
+    {
+        array_push($this->data, $data);
+    }
 
 }
